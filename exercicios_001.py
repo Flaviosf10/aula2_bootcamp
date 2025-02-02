40b138231b6e3c7bf2ebef238c92acc6824e39af
Flávio Linux <flaviosf10@hotmail.com>
2025-02-02 19:29:09 -0300

    Enviando exercicio 13

--- exercicios_001.py
+++ exercicios_001.py
@@ -11,7 +11,7 @@ total = print(f'A somo dos números é: {primeiro_numero + segundo_numero}')
 
 
 
-# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
