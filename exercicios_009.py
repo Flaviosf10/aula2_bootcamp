ed9c81d1855b0668dffb7deacb891c1fc01e9000
Flávio Linux <flaviosf10@hotmail.com>
2025-02-02 18:55:08 -0300

    Envio do exercicio 9

---
+++ exercicios_009.py
@@ -0,0 +1,5 @@
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+temperarura = float(input("digite a temperatura em Celcius: "))
+conversao = ((temperarura *1.8) + 32)
+print (f"A temperatura convertida em Fahrenheit é: {conversao: .2f}")
