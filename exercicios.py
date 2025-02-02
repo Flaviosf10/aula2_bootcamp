6279a47e634cce74911a514ed62f580c1c103125
Flavio De Souza Git aula <flaviosf10@hotmail.com>
2025-02-02 08:32:54 -0300

    Primeiros exercícios

---
+++ exercicios.py
@@ -0,0 +1,7 @@
+print(type(5/3))
+email = "flaviosf10@hotmail.com"
+nome_aluno = "Flávio de Souza Ferreira"
+print(type(nome_aluno))
+print(nome_aluno.upper())
+print(nome_aluno.lower())
+print(email.split("@"))
