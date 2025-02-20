daf2d31508e54931427dc8620597aface0fbeabd
Flávio Linux <flaviosf10@hotmail.com>
2025-02-20 18:47:52 -0300

    Retorno as aulas

--- exercicios_001.py
+++ exercicios_001.py
@@ -10,7 +10,7 @@ total = print(f'A somo dos números é: {primeiro_numero + segundo_numero}')
 
 # #### Booleanos (`bool`)
 
-# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
